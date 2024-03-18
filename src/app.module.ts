119e31a35a433dbebcd521605d06558ae408e695
Regazzoli Ignacio <ignacioregazzoli@gmail.com>
2024-03-18 16:07:47 -0300

    add TypeOrm and postgreSQL to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      logging: false,
+      entities: ['src/**/*.entity.ts'],
+      migrations: ['src/migration/**/*.ts'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
