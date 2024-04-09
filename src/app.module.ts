febe715a0e2d56ad2c47b823470d93692bbbb2ec
Regazzoli Ignacio <ignacioregazzoli@gmail.com>
2024-04-09 14:06:15 -0300

    Create Auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'dev-db', 
-      port: 5432,
-      username: 'admin',
-      password: '1234',
-      database: 'second_brain',
+      host: process.env.DB_HOST, 
+      port: +process.env.DB_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       synchronize: true,
       logging: false,
       entities: ['src/**/*.entity.ts'],
       migrations: ['src/migration/**/*.ts'],
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
